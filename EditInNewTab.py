cd4e95ca7a160787369f8bbc08ce44892f5bfa0e
Borja Campina <borjacampina@gmail.com>
2017-01-10 15:30:53 +0100

    Update to python code conventions

--- EditInNewTab.py
+++ EditInNewTab.py
@@ -2,14 +2,14 @@ import sublime, sublime_plugin
 
 class EditInNewTabCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        selectionText = ""
+        selection_text = ""
         # open a new file
         view = sublime.active_window().new_file()
 
         # get selected text
         sels = self.view.sel()
         for sel in sels:
-           selectionText = selectionText + self.view.substr(sel) + '\n'
+           selection_text = selection_text + self.view.substr(sel) + '\n'
 
         # insert selected text into the new file
-        view.insert(edit, 0, selectionText[:-1])
+        view.insert(edit, 0, selection_text[:-1])
